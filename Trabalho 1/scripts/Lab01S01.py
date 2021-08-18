f68472553edfcf859c3a822e1749a2043f2aa089
Guilherme <guilhergoa@hotmail.com>
2021-08-17 21:54:33 -0300

    Fix

--- Trabalho 1/scripts/Lab01S01.py
+++ Trabalho 1/scripts/Lab01S01.py
@@ -1,20 +1,33 @@
 # Arthur Rocha Amaral
 # Guilherme Oliveira Antônio
-
+# 
 # Lab01S01: Consulta graphql para 100 repositórios + requisição automática
-
-#Rodar comando para inciar ambiente virtual e acessa-lo.
-#    ``python3 - venv .venv``
-#    ``source .venv/bin/activate``
-#Instalar biblioteca usada.
-#    ``python -m pip install --pre gql[all]
+# 
+# Rodar comando para inciar ambiente virtual e acessa-lo.
+#    python3 - venv .venv
+#    source .venv/bin/activate
+# 
+# Instalar biblioteca usada.
+#    python -m pip install --pre gql[all]
+# 
+# Colocar um novo token gerado pelo github
+# https://docs.github.com/pt/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
+# 
+# Rodar
+#    pytthon scripts/Lab01S01.py -token TOKEN
 
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-token', help='token help', required=True)
+args = parser.parse_args()
 
 def Lab01S01():
+
     url = 'https://api.github.com/graphql'
-    token = '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'
+    token = args.token
 
     headers = {'Authorization': f'bearer {token}'}
 
