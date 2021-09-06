503b7055545766ea7ad02382064a76bbc4fadb9e
Guilherme <guilhergoa@hotmail.com>
2021-09-06 13:14:31 -0300

    fix  sh comands

--- Trabalho 2/scripts/analyse-repository.sh
+++ Trabalho 2/scripts/analyse-repository.sh
@@ -1,9 +1,12 @@
 WORK_DIR=files/analyses/$1
 
+echo Creating $WORK_DIR
 mkdir -p $WORK_DIR
 
 REPO_DIR=repositories/$1
 
+echo Analysing $REPO_DIR, result on $WORK_DIR
 java -jar ck.jar $REPO_DIR true 0 false $WORK_DIR
 
+echo removing $REPO_DIR
 rm -rf $REPO_DIR 
