7fc013d52959f2adc9eedc656fc4997ae6ca061d
Guilherme <guilhergoa@hotmail.com>
2021-09-12 10:11:58 -0300

    add files analysed - doing

--- Trabalho 2/scripts/analyse-repository.sh
+++ Trabalho 2/scripts/analyse-repository.sh
@@ -6,7 +6,7 @@ mkdir -p $WORK_DIR
 REPO_DIR=repositories/$1
 
 echo Analysing $REPO_DIR, result on $WORK_DIR
-java -jar ck.jar $REPO_DIR true 0 false $WORK_DIR
+java -jar ck.jar $REPO_DIR false 0 false $WORK_DIR
 
 echo removing $REPO_DIR
 rm -rf $REPO_DIR 
